14fefe9b78f5c5b01a2fee2c8ef1b782b78b7582
Vinc-78 <vincenzodicostanzo.um@gmail.com>
2021-10-27 17:02:47 +0200

    repo con file

---
+++ js/script.js
@@ -0,0 +1,143 @@
+// mostra i numeri estratti
+let numeriPc = numeriEstratti()
+
+let numeriUtente = []
+
+console.log(numeriPc)
+
+// parte il conteggio di 30 secondi
+
+ let contatore = 30;
+
+const clock = setInterval(() => {
+    console.log("mancano ", contatore, "secondi")
+    contatore--;
+
+    
+
+    if (contatore === 0) {
+
+        // azzera il timer e fa la verifica
+        clearInterval(clock);
+        verifica(numeriPc, numeriUtente);
+        
+
+    }
+
+}, 1000); 
+
+
+
+/* const chidi = setTimeout (myFunction, 30000);
+
+function myFunction() {
+    
+    
+} */
+
+// chiede i numeri nel mentre il conto alla rovescia va
+numeriUtente = ChiediNumeri()
+console.log(numeriUtente)
+
+
+
+
+// funzione che porta a schermo un allert di 5 numeri in forma leggibile 
+//e mantiene un array degli stessi umeri 
+
+function numeriEstratti() {
+
+    let numeriEstratti = [];
+
+    for (let i = 0; i < 5; i++) {
+        let estratto = numRandom();
+
+        numeriEstratti.push(estratto);
+    }
+
+
+    numeriDaVisualizzare = [...numeriEstratti];
+
+    numeriDaVisualizzare = numeriDaVisualizzare.toString()
+
+    numeriDaVisualizzare = numeriDaVisualizzare.replaceAll(",", "  ")
+
+
+    alert("hai 30 secondi, vediamo quanti numeri ricordi  i numeri che devi ricordare sono:    " + numeriDaVisualizzare)
+
+    return numeriEstratti;
+
+
+}
+
+
+
+//una funzione che estra un numero a caso da 1 a 100
+function numRandom() {
+    let num = Math.ceil(Math.random() * 100);
+    return num;
+}
+
+
+
+
+
+// funzione che chiede i numeri all'utente e controlla che siano numeri e che non siano ripetuti
+
+function ChiediNumeri() {
+
+    let arrayUtente = [];
+
+    for (let i = 0; i < 5; i++) {
+        let numero = prompt("Inserisci 5 numeri da 1 a 100");
+
+        if (isNaN(numero) || (numero > 100)) {
+            alert("il numero inserito non vale, !!! Attento che il tempo passa !!! ");
+            i--;
+        } else if (arrayUtente.includes(numero)) {
+            alert("numero già inserito, Inserisci un numero diverso")
+            i--;
+        }
+
+        else {
+            numero = parseInt(numero);
+            arrayUtente.push(numero);
+        }
+    }
+
+    return arrayUtente;
+
+
+}
+/**
+ * 
+ * @param {Array} arrayEstratti 
+ * @param {Array} arrayNdati 
+ */
+
+
+function verifica(arrayEstratti, arrayNdati) {
+
+    let punti = 0;
+
+    for (i = 0; i < arrayNdati.length; i++) {
+
+        if (arrayEstratti.includes(arrayNdati[i])) {
+            punti++;
+        }
+    }
+
+    console.log("Hai ricordato :", punti, "numeri")
+
+    if (punti === 0) {
+        alert("Non ne hai ricordato neanche uno :( Prova Ancora...")
+
+    } else if (punti < 5) {
+        alert("hai ricordato " + punti + " numeri ")
+
+    } else if (punti === 5) {
+        alert("!!! Complimenti li hai ricordati tutti !!! :) ")
+
+    }
+
+}
